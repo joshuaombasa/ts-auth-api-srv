8e1fa8f48d8242de83e9ed8ebdbd1574b1beb2ae
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-09 22:49:03 +0300

    initial commit

---
+++ auth/src/errors/custom-error.ts
@@ -0,0 +1,11 @@
+abstract class CustomError extends Error {
+  abstract statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+  }
+
+  abstract serializeError(): { message: string; field?: string }[];
+}
+
+export { CustomError };
