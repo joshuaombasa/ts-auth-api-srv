3d24d17e7ee62d07a14f978440dfefdf7dd6041a
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-09 22:49:29 +0300

    initial commit

---
+++ auth/src/errors/not-found-error.ts
@@ -0,0 +1,18 @@
+import { CustomError } from './custom-error';
+
+class NotFoundError extends CustomError {
+  statusCode = 404;
+
+  constructor() {
+    super('Route not found');
+
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+
+  serializeError(): { message: string; field?: string | undefined }[] {
+    return [{ message: 'Not found' }];
+  }
+}
+
+
+export {NotFoundError}
