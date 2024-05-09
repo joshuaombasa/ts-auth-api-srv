489280c88add4eee5b720195dcb407ebcd215e66
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-09 22:48:44 +0300

    initial commit

---
+++ auth/src/middleware/validate-request.ts
@@ -0,0 +1,20 @@
+import express, { NextFunction, Request, Response } from 'express';
+import {  validationResult } from 'express-validator';
+
+import { RequestValidationError } from '../errors/request-validation-error';
+
+const validateRequest = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(request);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+
+  next();
+};
+
+export { validateRequest };
