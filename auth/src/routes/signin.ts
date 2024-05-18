c1def092cb5bd42a565a89a8fe5ebeb7655bcd55
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-18 18:26:42 +0300

    initial commit

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,21 +1,18 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { BadRequestError } from '../errors/bad-request-error';
-
-import jwt from 'jsonwebtoken';
-import { Password } from '../services/password';
-
 import { validateRequest } from '../middleware/validate-request';
-
+import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 
 const signinRouter = express.Router();
 
 signinRouter.post(
   '/api/users/signin',
   [
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().notEmpty().withMessage('password must be provided'),
+    body('email').isEmail().withMessage('Email must be provided'),
+    body('password').trim().notEmpty().withMessage('Password must be provided'),
   ],
   validateRequest,
   async (request: Request, response: Response) => {
@@ -24,30 +21,29 @@ signinRouter.post(
     const existingUser = await User.findOne({ email });
 
     if (!existingUser) {
-      throw new BadRequestError('Invalid credentials');
+      throw new BadRequestError('Invalid Credentials');
     }
 
     const isPasswordsMatch = await Password.compare(
-      existingUser.password,
-      password
+      password,
+      existingUser?.password
     );
 
     if (!isPasswordsMatch) {
-      throw new BadRequestError('Invalid credentials');
+      throw new BadRequestError('Invalid Credentials');
     }
 
     const jwtToken = jwt.sign(
       {
-        id: existingUser._id.toString(),
         email: existingUser.email,
+        id: existingUser.id,
       },
       'asdf'
     );
 
     request.session = { jwt: jwtToken };
 
-    response.status(200).send(existingUser);
-
+    response.status(201).json({ existingUser });
   }
 );
 
