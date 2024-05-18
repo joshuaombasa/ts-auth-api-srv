676b3d8b6642e3046be63cf630ff1f1c4771b189
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-18 18:26:24 +0300

    initial commit

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,4 +1,4 @@
-import { scrypt, randomBytes } from 'crypto';
+import { randomBytes, scrypt } from 'crypto';
 import { promisify } from 'util';
 
 const scryptAsync = promisify(scrypt);
@@ -6,16 +6,15 @@ const scryptAsync = promisify(scrypt);
 class Password {
   static async toHash(password: string) {
     const salt = randomBytes(8).toString('hex');
-    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
-
-    return `${buf.toString('hex')}.${salt}`;
+    const buff = (await scryptAsync(password, salt, 64)) as Buffer;
+    return `${buff.toString('hex')}.${salt}`;
   }
 
-  static async compare(storedPassword: string, suppliedPassword: string) {
+  static async compare(suppliedPassword: string, storedPassword: string) {
     const [hashedPassword, salt] = storedPassword.split('.');
-    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+    const buff = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
-    return buf.toString('hex') === hashedPassword;
+    return buff.toString('hex') === hashedPassword;
   }
 }
 
