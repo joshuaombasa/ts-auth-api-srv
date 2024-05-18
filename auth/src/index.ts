1bd9f141bbe65b17a01708b43342ee5af42a7dd4
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-18 18:26:09 +0300

    initial commit

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,51 +1,50 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
 import cookieSession from 'cookie-session';
 
-const PORT = 3000;
-
-import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
 import { errorHandler } from './middleware/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { currentUserRouter } from './routes/current-user';
+
+const PORT = 4000;
 
 const app = express();
 
-app.set('trust proxy', true)
+app.set('trust proxy', true);
 
-app.use(cookieSession({
-  secure: false,
-  signed:false
-}))
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
 
 app.use(express.json());
 
-app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+app.use(currentUserRouter);
 
-app.all('*', () => {
-  throw new NotFoundError();
+app.all('*', (request: Request, response: Response) => {
+  throw new NotFoundError('Uknown endpoint');
 });
 
 app.use(errorHandler);
 
 const start = async () => {
-  try {
-    await mongoose.connect(
-      'mongodb://127.0.0.1:27017/auth4?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1'
-    );
-  } catch (error) {
-    console.error(error);
-  }
+  await mongoose.connect(
+    `mongodb://127.0.0.1:27017/tiketi7-auth?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1`
+  );
+  console.log('connected to mongodb');
 
   app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 };
 
-start()
+start();
