c721cf42edb54796778da744e6bea32d6c3b5dc7
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-18 18:26:34 +0300

    initial commit

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middleware/validate-request';
-
-import { User } from '../models/user';
+import { User, addUser } from '../models/user';
+import { Password } from '../services/password';
 
 const signupRouter = express.Router();
 
@@ -16,37 +15,37 @@ signupRouter.post(
     body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage(
-        'Password must have a length of between 4 and 20 characters.'
-      ),
+      .withMessage('Password must have a length of 4 to 20 characters'),
   ],
   validateRequest,
   async (request: Request, response: Response) => {
-
     const { email, password } = request.body;
 
-    await User.deleteMany({})
+    await User.deleteMany({});
 
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError('Email in use');
+      throw new BadRequestError('User with this email already exists');
     }
 
-    const userObject = User.build({ email, password });
-    const savedUser = await userObject.save();
+    const hashedPassword = await Password.toHash(password);
+
+    const UserObject = addUser({ email, password: hashedPassword });
+
+    const savedUser = await UserObject.save();
 
     const jwtToken = jwt.sign(
       {
-        id: savedUser._id.toString(),
         email: savedUser.email,
+        id: savedUser.id,
       },
       'asdf'
     );
 
     request.session = { jwt: jwtToken };
 
-    response.status(201).send(savedUser);
+    response.status(201).json({ savedUser });
   }
 );
 
