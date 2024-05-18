4e8e66660b05d334f9b66cae39882227a4de8c71
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-18 18:27:08 +0300

    initial commit

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,51 +1,28 @@
 import mongoose from 'mongoose';
-import { Password } from '../services/password';
 
 interface UserAttrs {
   email: string;
   password: string;
 }
 
-interface UserModel extends mongoose.Model<UserDoc> {
-  build(attrs: UserAttrs): UserDoc;
-}
-
-interface UserDoc extends mongoose.Document {
-  email: string;
-  password: string;
-}
-
-const userSchema = new mongoose.Schema(
-  {
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-  },
-  {
-    toJSON: {
-      transform(document, returnedObject) {
-        returnedObject.id = returnedObject._id.toString();
-
-        delete returnedObject._id;
-        delete returnedObject.__v;
-        delete returnedObject.password;
-      },
-    },
-  }
-);
-
-userSchema.pre('save', async function (done) {
-  if (this.isModified('password')) {
-    const passwordHash = await Password.toHash(this.get('password'));
-    this.set('password', passwordHash);
-  }
-
-  done();
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
-userSchema.statics.build = (attrs: UserAttrs) => {
-  return new User(attrs);
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+const addUser = (userData: UserAttrs) => {
+  return new User(userData);
 };
 
-const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
-
-export { User };
+export { User, addUser };
